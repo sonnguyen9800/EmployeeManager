54d13d1a2d465d21fdd5b1201c50065d6b5b2ba2
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-04-10 18:57:45 +0700

    Basically finished all task. Next is writting comment and clean code

--- extra.php
+++ extra.php
@@ -6,10 +6,23 @@ require_once 'php/google-api-php-client/vendor/autoload.php';
 <?php
 require_once('private/initialize.php');
 $page_title = "Extra";
-?>
+include(SHARED_PATH. '/header.php');
+$year_id = '';
+$str_query_time = "';";
+$all_employees = show_all_employees();
 
-<?php include(SHARED_PATH. '/header.php'); ?>
-<?php $all_employees = show_all_employees();
+if(is_post_request()) {
+    $year_id = $_POST['inputyear'];
+
+    if ($year_id == ''){
+        $str_query_time = "';";
+    }else{
+        $str_query_time = "' and Year=" . $year_id . ";";	
+    }
+} else {
+    $str_query_time = "';";
+}
+
 
 // Authentication and initialize
 $client = new Google_Client();
@@ -23,8 +36,7 @@ $request = new Google_Service_Bigquery_QueryRequest();
 //Make Query
 foreach($all_employees as $i_employee){
     $request->setQuery("#standardSQL \r\n SELECT SUM(Frequency) FROM `assignment1-cloudcomputing.assignment1.baby_name` WHERE Name='"
-		      .$i_employee->first_name.
-                       "' and Year=2012;");
+		      .$i_employee->first_name . $str_query_time );
     //Get the respone
     $response = $bigquery->jobs->query($projectId, $request);
     $rows = $response->getRows();
@@ -39,8 +51,7 @@ foreach($all_employees as $i_employee){
     $i_employee->first_name_freq=$str;
 
     $request->setQuery("#standardSQL \r\n SELECT SUM(Frequency) FROM `assignment1-cloudcomputing.assignment1.baby_name` WHERE Name='"
-		      .$i_employee->last_name.
-                       "' and Year=2012;");
+		      .$i_employee->last_name . $str_query_time  );
     //Get the respone
     $response = $bigquery->jobs->query($projectId, $request);
     $str='';
@@ -53,51 +64,52 @@ foreach($all_employees as $i_employee){
     }            
     $i_employee->last_name_freq=$str;       	
 
-
 };
 
 ?>
 <html>
     <?php
     $jumbotron_title = "Welcome";
-    $jumbotron_subtext = "See the frequency of the name";
+    if ($year_id != ''){
+	$jumbotron_subtext = "See the frequency of the name for the year" . $year_id;
+    } else {
+	$jumbotron_subtext = "See the frequency of the name in all time ";
+
+    }
     include(SHARED_PATH.'/jumbotron.php');
     ?>
 
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+    <form action="<?php echo '/extra'?>" method="POST" class="main-body container">
+        <div class="form-group">
+	    <label for="exampleInputEmail1">Type the year</label>
+	    <input type="number" class="form-control" id="inputYear"  placeholder="Enter the year" name="inputyear" value="<?php echo $year_id ?>">
+	    <small id="yearHelp" class="form-text text-muted">Enter any year, e.g: 2012. Default is all year. Keep this input blank to make it default. </small>
+	</div>
+	<button type="submit" class="btn btn-primary">Query</button>
+    </form>
 
+    <div class="container">
+	<table class="table main-body ">
+	    <thead>
+		<tr><th scope="col">ID</th>
+		    <th scope="col">First Name</th>
+		    <th scope="col">First Name Frequency</th>
+		    <th scope="col">Last Name</th>
+		    <th scope="col">Last Name Frequency</th>
+		</tr>
+	    </thead>
+	    <tbody>
+		<?php
+		foreach($all_employees as $i_employee){
+		    $employee = $i_employee;
+		    include(SHARED_PATH. '/card-freq.php');
+		}
+		?>
+	    </tbody>
+	</table>
 
-
-
-
-    <table class="table main-body container">
-	<thead>
-	    <tr><th scope="col">ID</th>
-		<th scope="col">First Name</th>
-		<th scope="col">First Name Frequency</th>
-		<th scope="col">Last Name</th>
-		<th scope="col">Last Name Frequency</th>
-	    </tr>
-	</thead>
-	<tbody>
-	    <?php
-	    foreach($all_employees as $i_employee){
-		$employee = $i_employee;
-		include(SHARED_PATH. '/card-freq.php');
-	    }
-	    ?>
-	</tbody>
-    </table>
-
+    </div>
+    
 </html>
 
 
